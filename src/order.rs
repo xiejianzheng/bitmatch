b838b59f4565a9bd4d53397a1906314baf7b31ba
xiejianzheng <xiejianzheng@gf.com.cn>
2019-06-28 18:03:30 +0800

    change orderbook structure

--- src/order.rs
+++ src/order.rs
@@ -1,6 +1,7 @@
 use std::collections::BTreeMap;
 use std::collections::VecDeque;
 
+
 pub enum Side {
         Bid,
         Ask,
@@ -8,6 +9,7 @@ pub enum Side {
 
 pub type Symbol = String;
 pub type OrderId = String;
+pub type OrderSeq = i32;
 pub type Price = i32;
 pub type Qty = i32;
 pub type OrderQueue = VecDeque<Order>;
@@ -21,6 +23,7 @@ pub enum OrdStatus {
 pub struct Order {
         symbol: Symbol,
         order_id: OrderId,
+        order_seq: OrderSeq,
         price: Price,
         order_qty: Qty,
         side: Side,
@@ -31,6 +34,26 @@ pub struct Order {
         ord_status: OrdStatus,
 }
 
+struct PriceTime {
+        price: Price,
+        time: OrderSeq,
+        ord_seq: OrderSeq,
+}
+
+impl Ord for PriceTime {
+        fn cmp(&self, other: &Self) -> Ordering {
+                (self.price, self.time).cmp(&(other.price, other.name))
+        }
+}
+
+impl PartialOrd for PriceTime {
+        fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+                Some(self.cmp(other))
+        }
+}
+
+impl Eq for PriceTime {
+}
 
 pub struct Match {
         symbol: Symbol,
@@ -42,21 +65,30 @@ pub struct Match {
 
 pub trait OrderBook {
         fn handle_order(&mut self, order: &mut Order) -> Option<Match>;
+        
+        fn try_match(order: &mut Order, queue_top: &mut Order) -> Option<Match> {
+                assert_ne!(order.Side, queue_top.Side);
+                
+        }
 }
 
-pub struct BTreeOrderBook {
-        bid_orders: BTreeMap<Price, OrderQueue>,
-        ask_orders: BTreeMap<Price, OrderQueue>,
+pub struct BinaryHeapOrderBook {
+        bid_heap: BinaryHeap<PriceTime>,
+        ask_heap: BinaryHeap<PriceTime>,
 }
 
-impl OrderBook for BTreeOrderBook {
+impl OrderBook for BinaryHeapOrderBook {
         
         fn handle_order(&mut self, order: &mut Order) -> Option<Match> {
                 let m: Option<Match>;
                 match order.side {
-                        Ask => 
-                               m = self.try_match(order, self.bid_orders),
-                        Bid => m = self.try_match(order, self.ask_orders),
+                        Ask => {
+                                m = self.try_match(order, self.bid_orders
+                        }
+
+                        Bid => {
+                                m = self.try_match(order, self.ask_orders.,
+                        }
                 }
                 
                 match order.ord_status {
@@ -68,16 +100,16 @@ impl OrderBook for BTreeOrderBook {
         }
 }
 
-impl BTreeOrderBook {
-        fn try_match(&mut self, mut order: &Order, otherSide: OrderQueue) -> Option<Match> {
-                order.ord_status = OrdStatus::New;
-                return Option::None;
-        }
+impl BinaryHeapOrderBook {
 
         fn enqueue(&mut self, order: Order) {
                 match order.side {
                         Ask => self.ask_orders.insert(order.price, order),
                         Bid => self.bid_orders.insert(order.price, order),
                 }
+        }
+
+        fn max(order_tree: &BTreeMap<Price, OrderQueue>) -> &OrderQueue {
+                order_tree.
         }
 }
